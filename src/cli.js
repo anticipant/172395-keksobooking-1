1560ff524358db43fb5a5320068f9c641c22b69a
d.shakhovich <lifeonline1>
2018-10-05 18:02:57 +0300

    принимаю только одну команду, остальные игнорю

--- src/cli.js
+++ src/cli.js
@@ -6,15 +6,12 @@ const packageInfo = require(`../package.json`);
 if (process.argv.length === 2) {
   console.log(`Привет пользователь! Эта программа будет запускать сервер «${packageInfo.name}». Автор: ${packageInfo.author}.`);
 } else {
-  process.argv.slice(2).forEach((it) => {
-    const command = it.slice(2);
-
-    if (cliInterface[command]) {
-      cliInterface[command].execute();
-    } else {
-      console.error(`Неизвестная команда {${it}}.
+  const command = process.argv[2].slice(2);
+  if (cliInterface[command]) {
+    cliInterface[command].execute();
+  } else {
+    console.error(`Неизвестная команда {${command}}.
 Чтобы прочитать правила использования приложения, наберите "--help"`);
-      process.exit(1);
-    }
-  });
+    process.exit(1);
+  }
 }
