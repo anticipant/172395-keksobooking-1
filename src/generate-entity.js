e13fc96a6585225b08021cfd7a54ff25dfb94a8f
anticipant <shakhovich.d@yandex.ru>
2018-10-09 22:52:55 +0300

    тесты к модулю генерации данных

--- src/generate-entity.js
+++ src/generate-entity.js
@@ -1,51 +1,81 @@
 'use strict';
 
-const item2 = {
-  "author": {
-    "avatar": `строка, адрес изображения, можно использовать https://robohash.org/{{random-string}}`,
+const userData = {
+  'avatar': `https://robohash.org/boom`,
+  'offerTitle': `Большая уютная квартира`,
+  'offerPrice': 100000,
+  'offerType': `flat`,
+  'offerRooms': 1,
+  'offerGuests': 2,
+  'offerCheckin': `sssssss`,
+  'offerFeatures': [`wi-fi`, `drinks`],
+  'offerPhotos': [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg'`],
+  'locationX': 500,
+  'locationY': 455,
+  'date': 100,
+};
+const itemTemplate = {
+  'author': {
+    'avatar': `строка, адрес изображения, можно использовать https://robohash.org/{{random-string}}`,
   },
 
-  "offer": {
-    "title": `строка, заголовок предложения, одно из фиксированных значений 
-              "Большая уютная квартира", 
-              "Маленькая неуютная квартира", 
-              "Огромный прекрасный дворец", 
-              "Маленький ужасный дворец", 
-              "Красивый гостевой домик", 
-              "Некрасивый негостеприимный домик", 
-              "Уютное бунгало далеко от моря", 
-              "Неуютное бунгало по колено в воде". 
+  'offer': {
+    'title': `строка, заголовок предложения, одно из фиксированных значений 
+              'Большая уютная квартира', 
+              'Маленькая неуютная квартира', 
+              'Огромный прекрасный дворец', 
+              'Маленький ужасный дворец', 
+              'Красивый гостевой домик', 
+              'Некрасивый негостеприимный домик', 
+              'Уютное бунгало далеко от моря', 
+              'Неуютное бунгало по колено в воде'. 
               Значения могут повторяться.`,
-    "address": `строка, адрес предложения, представляет собой запись вида "{{location.x}}, {{location.y}}"`,
-    "price": `число, случайная цена от 1000 до 1000000`,
-    "type": `строка с одним из четырёх фиксированных значений: flat, palace, house или bungalo`,
-    "rooms": `число, случайное количество комнат от 1 до 5`,
-    "guests": `число, случайное количество гостей, которое можно разместить`,
-    "checkin": `строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,`,
-    "checkout": `строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00`,
-    "features": `массив строк случайной длины из неповторяющихся элементов: "wifi", "dishwasher", "parking", "washer", "elevator", "conditioner",`,
-    "description": `пустая строка,`,
-    "photos": `массив из строк 
-              "http://o0.github.io/assets/images/tokyo/hotel1.jpg", 
-              "http://o0.github.io/assets/images/tokyo/hotel2.jpg" и 
-              "http://o0.github.io/assets/images/tokyo/hotel3.jpg" 
+    'address': `строка, адрес предложения, представляет собой запись вида '{{location.x}}, {{location.y}}'`,
+    'price': `число, случайная цена от 1000 до 1000000`,
+    'type': `строка с одним из четырёх фиксированных значений: flat, palace, house или bungalo`,
+    'rooms': `число, случайное количество комнат от 1 до 5`,
+    'guests': `число, случайное количество гостей, которое можно разместить`,
+    'checkin': `строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,`,
+    'checkout': `строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00`,
+    'features': `массив строк случайной длины из неповторяющихся элементов: 'wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner',`,
+    'description': `пустая строка,`,
+    'photos': `массив из строк 
+              'http://o0.github.io/assets/images/tokyo/hotel1.jpg', 
+              'http://o0.github.io/assets/images/tokyo/hotel2.jpg' и 
+              'http://o0.github.io/assets/images/tokyo/hotel3.jpg' 
               расположенных в произвольном порядке`,
   },
 
-  "location": {
-    "x": `случайное число, координата x метки на карте от 300 до 900,`,
-    "y": `случайное число, координата y метки на карте от 150 до 500`,
+  'location': {
+    'x': `случайное число, координата x метки на карте от 300 до 900,`,
+    'y': `случайное число, координата y метки на карте от 150 до 500`,
   },
-  "date": `число, дата размещения, представляет собой timestamp в формате UNIX. Представляет собой случайное число в интервале от сейчас минус 7 дней`,
+  'date': `число, дата размещения, представляет собой timestamp в формате UNIX. Представляет собой случайное число в интервале от сейчас минус 7 дней`,
 };
+console.log(typeof itemTemplate);
 
-console.log(typeof item2);
 const generateEntity = () => {
-  let item = {};
-  item.author = 1;
-  item.offer = 1;
-  item.location = 1;
-  item.date = 1;
-  return item;
+  return {
+    'author': {
+      'avatar': userData.avatar
+    },
+    'offer': {
+      'title': userData.offerTitle,
+      'price': userData.offerPrice,
+      'type': userData.offerType,
+      'rooms': userData.offerRooms,
+      'guests': userData.offerGuests,
+      'checkin': userData.offerCheckin,
+      'checkout': userData.offerCheckin,
+      'features': [1, 2],
+      'description': ``,
+      'photos': [``, ``, ``],
+    },
+    'location': {
+      'x': userData.locationX,
+      'y': userData.locationY,
+    },
+    'date': 1,
+  };
 };
 module.exports = generateEntity;
--- test/generate-entity.test.js
+++ test/generate-entity.test.js
@@ -2,15 +2,14 @@
 
 const assert = require(`assert`);
 const generateEntity = require(`../src/generate-entity`);
-console.log(`here`);
 
 describe(`Array`, () => {
-  describe(`generate entity`, () => {
+  describe(`main property`, () => {
     it(`should be the object`, () => {
       assert.equal(`object`, typeof generateEntity());
     });
-    it(`should be contain a property 'author'`, () => {
-      assert.equal(true, generateEntity().hasOwnProperty(`author`));
+    it(`should be contain a property 'offer'`, () => {
+      assert.equal(true, generateEntity().hasOwnProperty(`offer`));
     });
     it(`should be contain a property 'offer'`, () => {
       assert.equal(true, generateEntity().hasOwnProperty(`offer`));
@@ -20,6 +19,72 @@ describe(`Array`, () => {
     });
     it(`should be contain a property 'date'`, () => {
       assert.equal(true, generateEntity().hasOwnProperty(`date`));
+    });
+  });
+  describe(`sub property`, () => {
+    it(`should be the correct type`, () => {
+      assert.equal(`string`, typeof generateEntity().author.avatar);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`string`, typeof generateEntity().offer.title);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`number`, typeof generateEntity().offer.price);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`string`, typeof generateEntity().offer.type);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`number`, typeof generateEntity().offer.rooms);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`number`, typeof generateEntity().offer.guests);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`string`, typeof generateEntity().offer.checkin);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`string`, typeof generateEntity().offer.checkout);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(true, Array.isArray(generateEntity().offer.features));
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`string`, typeof generateEntity().offer.description);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(true, Array.isArray(generateEntity().offer.photos));
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`number`, typeof generateEntity().location.x);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`number`, typeof generateEntity().location.y);
+    });
+    it(`should be the correct type`, () => {
+      assert.equal(`number`, typeof generateEntity().date);
+    });
+  });
+  describe(`minimum requirements`, () => {
+    it(`avatar path should be contain 'https://robohash.org/'`, () => {
+      const index = generateEntity().author.avatar.indexOf(`https://robohash.org/`);
+      assert.equal(0, index);
+    });
+    it(`location x should be more/equal than 300 less/equal then 900`, () => {
+      assert.equal(true, generateEntity().location.x >= 300 && generateEntity().location.x <= 900);
+    });
+    it(`location y should be more/equal than 150 less/equal then 500`, () => {
+      assert.equal(true, generateEntity().location.y >= 150 && generateEntity().location.y <= 500);
+    });
+    it(`price should be more/equal than 1000 less/equal then 1000000`, () => {
+      assert.equal(true, generateEntity().offer.price >= 1000 && generateEntity().offer.price <= 1000000);
+    });
+    it(`rooms should be more/equal than 1 less/equal then 5`, () => {
+      assert.equal(true, generateEntity().offer.rooms >= 1 && generateEntity().offer.rooms <= 5);
+    });
+
+    it(`guests should be positive`, () => {
+      assert.equal(true, generateEntity().offer.guests >= 0);
     });
   });
 });
