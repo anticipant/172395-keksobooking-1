7c73b3d902c07fb786da25b785495b7ddad8e705
d.shakhovich <lifeonline1>
2018-10-29 13:49:38 +0300

    убрал лишнюю проверку

--- src/offers/validate.js
+++ src/offers/validate.js
@@ -28,8 +28,8 @@ const isInvalidTime = (time) => {
   const timeArray = time.split(`:`);
   const hours = parseInt(timeArray[0], 10);
   const minutes = parseInt(timeArray[1], 10);
-  const isHours = hours < 24 && hours >= 0 && timeArray[0].length === 2;
-  const isMinutes = minutes < 60 && minutes >= 0 && timeArray[1].length === 2;
+  const isHours = hours < 24 && hours >= 0;
+  const isMinutes = minutes < 60 && minutes >= 0;
 
   return !(isHours && isMinutes);
 };
