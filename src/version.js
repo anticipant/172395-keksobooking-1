82f3233a0496a6937735740dc84fdc4c6df90a0f
Dzmitry Shakhovich <shakhovich.d@yandex.ru>
2018-11-05 10:57:46 +0300

    В модуле version избавился от switch

--- src/version.js
+++ src/version.js
@@ -3,25 +3,13 @@
 const colors = require(`colors`);
 const packageInfo = require(`../package.json`);
 
+const versionParts = packageInfo.version.split(`.`);
+
 module.exports = {
   name: `version`,
   description: `печатает версию приложения`,
   execute() {
-    console.log(`v${packageInfo.version.split(`.`).map((it, index) => {
-      let number = it;
-      switch (index) {
-        case 0:
-          number = colors.red(it);
-          break;
-        case 1:
-          number = colors.green(it);
-          break;
-        case 2:
-          number = colors.blue(it);
-          break;
-      }
-      return number;
-    }).join(`.`)}`);
+    console.log(`v${colors.red(versionParts[0])}.${colors.green(versionParts[1])}.${colors.blue(versionParts[2])}`);
     process.exit(0);
   }
 };
