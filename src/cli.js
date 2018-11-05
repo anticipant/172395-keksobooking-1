a31c41cdc9f2e878ad93f84e4a7ade1fcfafece4
Dzmitry Shakhovich <shakhovich.d@yandex.ru>
2018-11-05 10:39:42 +0300

    добавил procces.exit в CLI

--- src/cli.js
+++ src/cli.js
@@ -6,6 +6,7 @@ const packageInfo = require(`../package.json`);
 
 if (process.argv.length === 2) {
   console.log(colors.green(`Привет пользователь! Эта программа будет запускать сервер «${packageInfo.name}». Автор: ${packageInfo.author}.`));
+  process.exit(0);
 } else {
   const [, , command, ...parameters] = process.argv;
   const commandName = command.slice(2);
